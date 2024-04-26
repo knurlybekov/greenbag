99282d0682a0e32ab812d143f4f4d1039816e5e6
Karen Nurlybekov <nurlybekov.karen@gmail.com>
2024-04-25 19:35:35 -0700

    added login and signup

--- greenbag/urls.py
+++ greenbag/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView
+
+from .views import SignUp
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('signup/', SignUp.as_view(), name='signup')
 ]
--- greenbag/views.py
+++ greenbag/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render, HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
